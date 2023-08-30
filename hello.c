195c4df6075a6dae40ce29bb70d69136dfb2d38c
Near <andy861117@gmail.com>
2023-08-30 11:20:49 +0800

    update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
-  printf("Hello world!\n");
-  retrun 0;
+int main(){
+	printf("Hello world!\n");
+
+	for(int i = 0; i < 5; i++){
+		printf("i: %d\n", i);
+	}	// End of for-loop
+	return 0;
 } // End of main
